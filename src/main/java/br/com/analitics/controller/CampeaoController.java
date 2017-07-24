134eae16bd6e7c9ec7e72713b9c8bdb3e9c35d9e
Richard Martins <richard.martins@lello.com.br>
2017-07-24 17:13:05 -0300

    rest controller

--- src/main/java/br/com/analitics/controller/CampeaoController.java
+++ src/main/java/br/com/analitics/controller/CampeaoController.java
@@ -8,14 +8,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.analitics.business.CampeaoBusiness;
 import br.com.analitics.model.Campeao;
 import br.com.analitics.vo.CampeaoListaVO;
 
 @Controller
+@RestController
 @RequestMapping("/campeao")
 public class CampeaoController {
 	
@@ -24,13 +25,13 @@ public class CampeaoController {
 	
 	@ResponseStatus(value = HttpStatus.OK)
 	@RequestMapping(value = "{id}", method = RequestMethod.GET)
-	public @ResponseBody Campeao findByIdRito(@PathVariable("id") int idRito){
+	public Campeao findByIdRito(@PathVariable("id") int idRito){
         return campeaoBusiness.findByIdRito(idRito);
 	}
 	
 	@ResponseStatus(value = HttpStatus.OK)
 	@RequestMapping(value = "lista", method = RequestMethod.GET)
-	public @ResponseBody List<CampeaoListaVO> findAllLista() {
+	public List<CampeaoListaVO> findAllLista() {
 		return campeaoBusiness.findAllLista();
 	}
 
